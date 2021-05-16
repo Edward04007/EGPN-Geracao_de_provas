bdeb7b8d45698b3356586b40326a825caa76a279
Éverton <foreverplayer95@gmail.com>
2021-05-15 23:37:59 -0300

    api

--- src/Service/api.jsx
+++ src/Service/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://ed-egpn-jonas.herokuapp.com/api/',
+    baseURL: 'https://egpn-banco-de-questoes.herokuapp.com/api/',
     timeout: 2000,
     headers: { 'Content-Type': 'application/json',
               'Accept': 'application/json'
